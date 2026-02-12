6b0cfd4bfd91c4c2ed4dd28ea8ccc27c311d2a37
ahmed-mohamed-portfolio <ahmed.mohamed.connect@gmail.com>
2026-02-12 11:07:46 +0200

    env

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,20 +1,21 @@
 
-import {mongoose} from 'mongoose';
+import { mongoose } from 'mongoose';
 import { DB_URL } from "../../config/index.js";
 
 
 
-export const databaseConnection =async () => {
+export const databaseConnection = async () => {
 
-try { 
-    const databaseConnectionResult = await mongoose.connect(DB_URL)
-    console.log("mongoose connected successfully");
-    
-    
-} catch (error) {
-    console.log(`fail to connect to DB ${error}`);
-    
-}
-    
+    try {
+
+        const databaseConnectionResult = await mongoose.connect(DB_URL)
+        console.log("mongoose connected successfully");
+
+    } catch (error) {
+
+        console.log(`fail to connect to DB ${error}`);
+
+    }
+
 
 }
